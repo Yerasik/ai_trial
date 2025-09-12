8cd5bfdc1b53e88e52d69c4d01386d25b7bb08e5
Yerasik <yerassylt@connect.hku.hk>
2025-09-12 17:44:10 +0800

    connected a random dataset to the draft neural network

--- network.py
+++ network.py
@@ -3,30 +3,22 @@ from nnfs.datasets import spiral_data
 import random
 import matplotlib.pyplot as plt
 import nnfs
-# to show the plot of the Data set
-#
-# X, y = spiral_data(samples= 100, classes=5)
-# plt.scatter(X[:,0], X[:,1], c=y, cmap='brg') 
-# plt.show()
 
-# to calculate the layers of the neural network
-#
-# inputs = [[1.0, 2.0, 3.0, 2.5],[2.0, 5.0,-1.0, 2.0],[-1.5,2.7, 3.3,-0.8]]
-# weights = [[0.2, 0.8, -0.5, 1.0],[0.5, -0.91, 0.26, -0.5],[-0.26, -0.27, 0.17, 0.87]]
-# biases = [2.0, 3.0, 0.5]
-# weights2 = [[0.1, -0.14, 0.5],[-0.5, 0.12, -0.331],[-0.44, 0.73, -0.131]]
-# biases2 = [-1, 2, -0.5]
-# layer1_outputs = np.dot (inputs, np.array (weights) .T) + biases
-# layer2_outputs = np.dot (layer1_outputs, np.array (weights2) .T) + biases2
 
-# print(layer2_outputs)
-
-class Layer Dense:
-    def _init__(self, n_inputs, n_neurons) :
-        def __init__(self, n_inputs, n_neurons):
+class Layer_Dense:
+    # Layer initialization
+    def __init__(self, n_inputs, n_neurons):
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons) 
         self.biases = np.zeros((1, n_neurons))
     # Forward pass
     def forward(self, inputs):
-    # Calculate output values from inputs, weights and biases
-    pass # using pass statement as a placeholder
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+# Create dataset
+X, y = spiral_data(samples=100, classes=3)
+# Create Dense layer with 2 input features and 3 output values
+dense1 = Layer_Dense (2, 3)
+# Perform a forward pass of our training data through this layer
+dense1.forward(X)
+# Let's see output of the first few samples:
+print(dense1.output[:5])
