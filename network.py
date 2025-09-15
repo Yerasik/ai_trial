b4bf7ff1890d9ef9a58f61182ad803514a58cf75
Yerasik <yerassylt@connect.hku.hk>
2025-09-15 14:34:51 +0800

    the second protoype of the ReLU activation function

--- network.py
+++ network.py
@@ -26,8 +26,5 @@ import nnfs
 inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
 output = []
 for i in inputs:
-    if i > 0:
-        output.append (i)
-    else:
-        output. append(0)
+    output.append(max(0, i))
 print(output)
