fc73e662ecb0c4481bdfb33f4eb1bc65d20ce636
Yerasik <yerassylt@connect.hku.hk>
2025-09-15 16:19:22 +0800

    Softmax function operation draft

--- network.py
+++ network.py
@@ -20,16 +20,37 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
-# Create dataset
-X, y = spiral_data(samples=100, classes=3)
-# Create Dense layer with 2 input features and 3 output values
-dense1 = Layer_Dense (2, 3)
-# Create ReLU activation (to be used with Dense layer):
-activation1 = Activation_ReLU()
-# Make a forward pass of our training data through this layer
-dense1.forward(X)
-# Forward pass through activation func.
-# Takes in output from previous layer
-activation1.forward(dense1.output)
- # Let's see output of the first few samples:
-print(activation1.output[:5])
+# # Create dataset
+# X, y = spiral_data(samples=100, classes=3)
+# # Create Dense layer with 2 input features and 3 output values
+# dense1 = Layer_Dense (2, 3)
+# # Create ReLU activation (to be used with Dense layer):
+# activation1 = Activation_ReLU()
+# # Make a forward pass of our training data through this layer
+# dense1.forward(X)
+# # Forward pass through activation func.
+# # Takes in output from previous layer
+# activation1.forward(dense1.output)
+#  # Let's see output of the first few samples:
+# print(activation1.output[:5])
+
+
+layer_outputs = [4.8, 1.21, 2.385]
+# e - mathematical constant, we use E here to match a common coding
+# style where constants are uppercased
+E = 2.71828182846 # you can also use math.e
+# For each value in a vector, calculate the exponential value
+exp_values = []
+for output in layer_outputs:
+    exp_values.append(E ** output) # ** - power operator in Python
+print("exponentiated values:")
+print (exp_values)
+
+# Now normalize values
+norm_base = sum(exp_values) # We sum all values
+norm_values = []
+for value in exp_values:
+    norm_values.append(value / norm_base)
+print('Normalized exponentiated values:')
+print (norm_values)
+print('Sum of normalized values:' , sum (norm_values))
