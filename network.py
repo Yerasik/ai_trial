434da20ae61f9a3fe712759759e29b96ee07b7da
Yerasik <yerassylt@connect.hku.hk>
2025-09-15 14:41:05 +0800

    added an Activation_ReLU class

--- network.py
+++ network.py
@@ -4,27 +4,32 @@ import random
 import matplotlib.pyplot as plt
 import nnfs
 
+# ReLU activation
+class Activation_ReLU:
+    # Forward pass
+    def forward (self, inputs):
+        # Calculate output values from input
+        self.output = np.maximum (0, inputs)
 
-# class Layer_Dense:
-#     # Layer initialization
-#     def __init__(self, n_inputs, n_neurons):
-#         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons) 
-#         self.biases = np.zeros((1, n_neurons))
-#     # Forward pass
-#     def forward(self, inputs):
-#         self.output = np.dot(inputs, self.weights) + self.biases
+class Layer_Dense:
+    # Layer initialization
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons) 
+        self.biases = np.zeros((1, n_neurons))
+    # Forward pass
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-# # Create dataset
-# X, y = spiral_data(samples=100, classes=3)
-# # Create Dense layer with 2 input features and 3 output values
-# dense1 = Layer_Dense (2, 3)
-# # Perform a forward pass of our training data through this layer
-# dense1.forward(X)
-# # Let's see output of the first few samples:
-# print(dense1.output[:5])
-
-inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
-output = []
-for i in inputs:
-    output.append(max(0, i))
-print(output)
+# Create dataset
+X, y = spiral_data(samples=100, classes=3)
+# Create Dense layer with 2 input features and 3 output values
+dense1 = Layer_Dense (2, 3)
+# Create ReLU activation (to be used with Dense layer):
+activation1 = Activation_ReLU()
+# Make a forward pass of our training data through this layer
+dense1.forward(X)
+# Forward pass through activation func.
+# Takes in output from previous layer
+activation1.forward(dense1.output)
+ # Let's see output of the first few samples:
+print(activation1.output[:5])
