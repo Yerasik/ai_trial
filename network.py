c2fa5c05d01a0f8c3514443d87a61f7da6d01788
Yerasik <yerassylt@connect.hku.hk>
2025-09-15 19:42:19 +0800

    added the loss function

--- network.py
+++ network.py
@@ -29,6 +29,48 @@ class Activation_Softmax:
         # Normalize them for each sample
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
+# Common loss class
+class Loss:
+    # Calculates the data and regularization losses
+    # given model output and ground truth values
+    def calculate (self, output, y):
+    # Calculate sample losses
+    sample_losses = self.forward(output, y)
+    # Calculate mean loss
+    data_loss = np. mean (sample_losses)
+    # Return loss
+    return data_loss
+
+# Cross-entropy loss
+class Loss_CategoricalCrossentropy(Loss):
+    # Forward pass
+    def forward(self, y_pred, y_true):
+
+        # Number of samples in a batch
+        samples = len (y_pred)
+
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        # Probabilities for target values
+        # only if categorical labels
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[
+            range(samples), 
+            y_true
+        ]
+
+        # Mask values - only for one-hot encoded labels
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(
+            y_pred_clipped * y_true,
+            axis=1
+            )
+
+        # Losses
+        negative log likelihoods = -np.log(correct confidences)
+        return negative log likelihoods
 
 # Create dataset
 X, y = spiral_data(samples=100, classes=3)
@@ -44,7 +86,10 @@ activation1.forward(dense1.output)
 dense2.forward(activation1.output)
 activation2.forward(dense2.output)
 
-#print the result
-print(activation2.output[:5])
+#print the result of the loss
+
+loss_function = Loss_CategoricalCrossentropy
+loss = loss_function.calculate (softmax_outputs, class_targets)
+print (loss)
 
 
