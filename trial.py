2a43a84bb7533d9456ec6da6b5021a4deaaf12a0
Yerasik <yerassylt@connect.hku.hk>
2025-09-17 19:52:01 +0800

    calculated the derivatives of all inputs

--- trial.py
+++ trial.py
@@ -91,13 +91,13 @@ b = 1.0 # bias
 xw0 = x[0] * w[0]
 xw1 = x[1] * w[1]
 xw2 = x[2] * w[2]
-print (xw0, xw1, xw2, b)
+
 # Adding weighted inputs and a bias
 z = xw0 + xw1 + xw2 + b
-print(z)
+
 # ReLU activation function
 y = max(z, 0)
-print (y)
+
 
 # Backward pass
 # The derivative from the next layer
@@ -105,3 +105,31 @@ dvalue = 1.0
 # Derivative of ReLU and the chain rule
 drelu_dz = dvalue * (1. if z > 0 else 0. )
 print(drelu_dz)
+
+# Partial derivatives of the multiplication, the chain rule
+dsum_dxw0 = 1
+dsum_dxw1 = 1
+dsum_dxw2 = 1
+dsum_db = 1
+drelu_dxw0 = drelu_dz * dsum_dxw0
+drelu_dxw1 = drelu_dz * dsum_dxw1
+drelu_dxw2 = drelu_dz * dsum_dxw2
+drelu_db = drelu_dz * dsum_db
+print(drelu_dxw0, drelu_dxw1, drelu_dxw2, drelu_db)
+
+# Partial derivatives of the multiplication, the chain rule
+dmul_dx0 = w[0]
+dmul_dx1 = w[1]
+dmul_dx2 = w[2]
+dmul_dwo = x[0]
+dmul_dw1 = x[1]
+dmul_dw2 = x[2]
+drelu_dx0 = drelu_dxw0 * dmul_dx0
+drelu_dw0 = drelu_dxw0 * dmul_dwo
+drelu_dx1 = drelu_dxw1 * dmul_dx1
+drelu_dw1 = drelu_dxw1 * dmul_dw1
+drelu_dx2 = drelu_dxw2 * dmul_dx2
+drelu_dw2 = drelu_dxw2 * dmul_dw2
+print(drelu_dx0, drelu_dw0, drelu_dx1, drelu_dw1, drelu_dx2, drelu_dw2)
+
+# chapter 9, page 200
