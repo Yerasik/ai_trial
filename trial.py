4076230d56f55653624866ef291384ff7d2f88f8
Yerasik <yerassylt@connect.hku.hk>
2025-09-18 15:55:12 +0800

    changed the dimensions of the output

--- trial.py
+++ trial.py
@@ -89,14 +89,12 @@ loss_function = Loss_CategoricalCrossentropy ()
 dvalues = np.array ([[1., 1., 1.],
 [2., 2., 2.],
 [3., 3., 3.]])
-# We have 3 sets of weights - one set for each neuron
-# we have 4 inputs, thus 4 weights
-# recall that we keep weights transposed
-weights = np. array([ [0.2, 0.8, -0.5, 1],
-[0.5, -0.91, 0.26, -0.5],
-[-0.26, -0.27, 0.17, 0.87]]).T
+# We have 3 sets of inputs - samples
+inputs = np.array([[1, 2, 3, 2.5], [2., 5., -1., 2],
+[-1.5, 2.7, 3.3, -0.8]])
 
 # sum weights of given input
-# and multiply by the passed in gradient for this neuron
-dinputs = np.dot (dvalues, weights.T)
-print(dinputs)
+# and multiply by the passed in gradient for this neuron 
+dweights = np.dot(inputs.T, dvalues)
+
+print(dweights)
