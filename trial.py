56eb21d7c00eb889fa2e253741e1e50e4a8f46a9
Yerasik <yerassylt@connect.hku.hk>
2025-09-18 16:16:20 +0800

    added the whole Relu function output optimization draft code

--- trial.py
+++ trial.py
@@ -83,18 +83,48 @@ activation2 = Activation_Softmax()
 # Create loss function
 loss_function = Loss_CategoricalCrossentropy ()
 
+
+
 # Passed in gradient from the next layer
 # for the purpose of this example we're going to use
 # an array of an incremental gradient values
-dvalues = np.array ([[1., 1., 1.],
+dvalues = np.array([[1., 1., 1.],
 [2., 2., 2.],
 [3., 3., 3.]])
 # We have 3 sets of inputs - samples
-inputs = np.array([[1, 2, 3, 2.5], [2., 5., -1., 2],
+inputs = np.array([[1, 2, 3, 2.5],
+[2., 5., -1., 2],
 [-1.5, 2.7, 3.3, -0.8]])
+# We have 3 sets of weights - one set for each neuron
+# we have 4 inputs, thus 4 weights
+# recall that we keep weights transposed
+weights = np.array([[0.2, 0.8, -0.5, 1],
+[0.5, -0.91, 0.26, -0.5],
+[-0.26, -0.27, 0.17, 0.87]]).T
 
-# sum weights of given input
-# and multiply by the passed in gradient for this neuron 
-dweights = np.dot(inputs.T, dvalues)
+# One bias for each neuron
+# biases are the row vector with a shape (1, neurons)
+biases = np.array([[2, 3, 0.5]])
+# Forward pass
+layer_outputs = np.dot (inputs, weights) + biases # Dense layer
+relu_outputs = np.maximum(0, layer_outputs) # ReLU activation
+# Let's optimize and test backpropagation here
+# ReLU activation - simulates derivative with respect to input values
+# from next layer passed to current layer during backpropagation
+drelu = relu_outputs.copy()
+drelu[layer_outputs <= 0] = 0
+# Dense layer
+# dinputs - multiply by weights
+dinputs = np.dot (drelu, weights.T)
+# dweights - multiply by inputs
+dweights = np.dot(inputs.T, drelu)
+# biases - sum values, do this over samples (first axis), keepdims
+# since this by default will produce a plain list -
+# we explained this in the chapter 4
+dbiases = np.sum(drelu, axis=0, keepdims=True)
+# Update parameters
+weights += -0.001 * dweights 
+biases += -0.001 * biases
 
-print(dweights)
+print(weights)
+print(biases)
