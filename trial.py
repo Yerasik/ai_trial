bd6e70ae885a2f7461df0536ba29010856d14ff8
Yerasik <yerassylt@connect.hku.hk>
2025-09-18 15:49:07 +0800

    added a batch of inputs to see the results of the gradients

--- trial.py
+++ trial.py
@@ -83,24 +83,20 @@ activation2 = Activation_Softmax()
 # Create loss function
 loss_function = Loss_CategoricalCrossentropy ()
 
-import numpy as np
 # Passed in gradient from the next layer
 # for the purpose of this example we're going to use
-# a vector of 1s
-dvalues = np.array([[1., 1., 1.]])
+# an array of an incremental gradient values
+dvalues = np.array ([[1., 1., 1.],
+[2., 2., 2.],
+[3., 3., 3.]])
 # We have 3 sets of weights - one set for each neuron
 # we have 4 inputs, thus 4 weights
 # recall that we keep weights transposed
-weights = np.array([[0.2, 0.8, -0.5, 1],
+weights = np. array([ [0.2, 0.8, -0.5, 1],
 [0.5, -0.91, 0.26, -0.5],
 [-0.26, -0.27, 0.17, 0.87]]).T
+
 # sum weights of given input
 # and multiply by the passed in gradient for this neuron
-dx0 = sum(weights [0])*dvalues[0]
-dx1 = sum(weights [1])*dvalues[0]
-dx2 = sum(weights [2])*dvalues[0]
-dx3 = sum(weights [3])*dvalues[0]
-dinputs = np.array([dx0, dx1, dx2, dx3])
+dinputs = np.dot (dvalues, weights.T)
 print(dinputs)
-
-# chapter 9, page 200
