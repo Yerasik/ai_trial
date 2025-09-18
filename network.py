eaeba9aa43fc2d1c11a091707923a9e479db4c8c
Yerasik <yerassylt@connect.hku.hk>
2025-09-18 16:44:02 +0800

    added the backward pass function

--- network.py
+++ network.py
@@ -9,6 +9,13 @@ class Activation_ReLU:
     def forward (self, inputs):
         # Calculate output values from input
         self.output = np.maximum (0, inputs)
+    # Backward pass
+    def backward(self, dvalues):
+        # Since we need to modify the original variable,
+        # let's make a copy of the values first
+        self.dinputs = dvalues.copy()
+        # Zero gradient where input values were negative
+        self.dinputs [self.inputs <= 0] = 0
 
 class Layer_Dense:
     # Layer initialization
@@ -18,6 +25,14 @@ class Layer_Dense:
     # Forward pass
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
+        self.inputs = inputs
+    # Backward pass
+    def backward(self, dvalues):
+        # Gradients on parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        # Gradient on values
+        self.dinputs = np.dot(dvalues, self.weights.T)
 
 # Softmax activation
 class Activation_Softmax:
