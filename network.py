1051fd8f08deb1f20d00e7e384e6f2c198e4fb38
Yerasik <yerassylt@connect.hku.hk>
2025-09-22 13:06:10 +0800

    added a separate faster backpropagation technique

--- network.py
+++ network.py
@@ -122,6 +122,38 @@ class Loss_CategoricalCrossentropy(Loss):
         # Normalize gradient
         self.dinputs = self.dinputs / samples
 
+# Softmax classifier - combined Softmax activation
+# and cross-entropy loss for faster backward step
+class Activation_Softmax_Loss_CategoricalCrossentropy():
+    # Creates activation and loss function objects
+    def __init__(self):
+        self. activation = Activation_Softmax ()
+        self.loss = Loss_CategoricalCrossentropy()
+
+    # Forward pass
+    def forward (self, inputs, y_true):
+        # Output layer's activation function
+        self.activation.forward(inputs)
+        # Set the output
+        self.output = self.activation.output
+        # Calculate and return loss value
+        return self.loss.calculate(self.output, y_true)
+    
+    # Backward pass
+    def backward (self, dvalues, y_true):
+        # Number of samples
+        samples = len(dvalues)
+        # If labels are one-hot encoded,
+        # turn them into discrete values
+        if len(y_true.shape) == 2:
+            y_true = np.argmax(y_true, axis=1)
+        
+        # Copy so we can safely modify
+        self.dinputs = dvalues.copy()
+        # Calculate gradient
+        self.dinputs[range(samples), y_true] -= 1
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
 
 nnfs.init()
 # Create dataset
