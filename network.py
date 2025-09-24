2f206de2b5e0cdc3e20ac08332e5f0814abac691
Yerasik <yerassylt@connect.hku.hk>
2025-09-24 14:47:46 +0800

    addde an implementation of the RMSprop optimizer:

--- network.py
+++ network.py
@@ -2,12 +2,14 @@ import matplotlib.pyplot as plt
 import nnfs
 from nnfs.datasets import spiral_data
 import numpy as np
-
+nnfs.init()
 # ReLU activation
 class Activation_ReLU:
     # Forward pass
     def forward (self, inputs):
-        # Calculate output values from input
+        # Remember input values
+        self.inputs = inputs
+        # Calculate output values from inputs
         self.output = np.maximum (0, inputs)
     
     # Backward pass
@@ -52,18 +54,14 @@ class Activation_Softmax:
         # Create uninitialized array
         self.dinputs = np.empty_like(dvalues)
         # Enumerate outputs and gradients
-        for index, (single_output, single_dvalues) in \
-            enumerate(zip(self.output, dvalues)):
-
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
             # Flatten output array
             single_output = single_output.reshape(-1, 1)
             # Calculate Jacobian matrix of the output and
-            jacobian_matrix = np.diagflat (single_output) - \
-            np.dot (single_output, single_output.T)
+            jacobian_matrix = np.diagflat (single_output) - np.dot (single_output, single_output.T)
             # Calculate sample-wise gradient
             # and add it to the array of sample gradients
-            self. dinputs [index] = np.dot (jacobian_matrix,
-            single_dvalues)
+            self. dinputs [index] = np.dot (jacobian_matrix,single_dvalues)
 
 # Common loss class
 class Loss:
@@ -156,59 +154,92 @@ class Activation_Softmax_Loss_CategoricalCrossentropy():
         self.dinputs = self.dinputs / samples
 
 class Optimizer_SGD:
+   
     # Initialize optimizer - set settings,
     # learning rate of 1. is default for this optimizer
-    def _init_(self, Learning_rate=1.0):
-        self.Iearning_rate = learning_rate
-    # Update parameters
-    def update_params (self, Layer):
-        layer. weights += -self. learning_rate * layer.dweights 
-        layer.biases += -self.learning_rate * layer.dbiases
+    def __init__(self, learning_rate=1., decay=0., momentum=0.):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.momentum = 0
 
-nnfs.init()
+    # Call once before any parameter updates
+    def pre_update_params (self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1. / (1. + self. decay * self.iterations))
+    # Update parameters
+    def update_params (self, layer):
+         # If we use momentum
+        if self.momentum:
+            # If layer does not contain momentum arrays, create them
+            # filled with zeros
+            if not hasattr (layer,'weight_momentums' ):
+                layer.weight_momentums = np.zeros_like(layer.weights)
+                # If there is no momentum array for weights
+                # The array doesn't exist for biases yet either.
+                layer.bias_momentums = np.zeros_like(layer.biases)
+
+            # Build weight updates with momentum - take previous
+            # updates multiplied by retain factor and update with
+            # current gradients
+            weight_updates = self.momentum * layer.weight_momentums - self.current_learning_rate * layer.dweights
+            layer.weight_momentums = weight_updates
+            # Build bias updates
+            bias_updates = self.momentum * layer.bias_momentums - self.current_learning_rate * layer.dbiases
+            layer.bias_momentums = bias_updates
+        else:
+            weight_updates = - self.current_learning_rate * layer.dweights
+            bias_updates = - self.current_learning_rate * layer.dbiases
+
+
+        layer.weights += weight_updates
+        layer.biases += bias_updates
+    # Call once after any parameter updates
+    def post_update_params (self):
+        self.iterations += 1
+
 # Create dataset
 X, y = spiral_data(samples=100, classes=3)
-
-# Create model
-dense1 = Layer_Dense (2, 3) # first dense layer, 2 inputs
+# Create Dense layer with 2 input features and 64 output values
+dense1 = Layer_Dense (2, 64)
+# Create ReLU activation (to be used with Dense layer):
 activation1 = Activation_ReLU()
-dense2 = Layer_Dense (3, 3) # second dense layer, 3 inputs, 3 outputs
+# Create second Dense layer with 64 input features (as we take output
+# of previous layer here) and 3 output values (output values)
+dense2 = Layer_Dense (64, 3)
 # Create Softmax classifier's combined loss and activation
 loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy ()
+# Create optimizer
+optimizer = Optimizer_SGD(decay=1e-3, momentum=0.9)
 
-# Perform a forward pass 
-dense1.forward (X)
-# Perform a forward pass throught an activation function
-activation1.forward(dense1.output)
-# Perfrom forward pass thorugh the second layer
-dense2.forward(activation1.output) 
-
-# Calculate sample losses from output of activation2 (softmax activation)
-loss = loss_activation.forward(dense2.output, y)
-    
-# Print the output of first samples
-print(loss_activation.output[:5])
-# Print Loss value
-print ('loss:',loss)
-
-# Calculate accuracy from output of activation2 and targets
-# calculate values along first axis
-predictions = np.argmax(loss_activation.output, axis=1)
-if len(y.shape) == 2:
-    y = np.argmax(y, axis=1)
-accuracy = np.mean(predictions==y)
-
-# Print accuracy
-print('acc:',accuracy)
-
-# Backward pass
-loss_activation.backward(loss_activation.output, y) 
-dense2.backward(loss_activation.dinputs)
-activation1.backward(dense2.dinputs)
-dense1.backward (activation1.dinputs)
-
-# Print gradients
-print (dense1.dweights)
-print(dense1.dbiases)
-print (dense2.dweights)
-print (dense2.dbiases)
+for epoch in range(10001):
+    # Perform a forward pass of our training data through this layer
+    dense1.forward (X)
+    # Perform a forward pass through activation function
+    # takes the output of first dense layer here
+    activation1.forward(dense1.output)
+    # Perform a forward pass through second Dense layer
+    # takes outputs of activation function of first layer as inputs
+    dense2.forward(activation1.output)
+    # Perform a forward pass through the activation/loss function
+    # takes the output of second dense layer here and returns loss
+    loss = loss_activation.forward(dense2.output, y)
+    # Calculate accuracy from output of activation2 and targets
+    # calculate values along first axis
+    predictions = np.argmax(loss_activation.output, axis=1)
+    if len(y.shape) == 2:
+        y = np.argmax (y, axis=1)
+    accuracy = np.mean(predictions==y)
+    if not epoch % 100:
+        print(f'epoch: {epoch}, ' + f'acc: {accuracy: 3f}, ' + f'loss: {loss: .3f},' +f'lr: {optimizer.current_learning_rate} ' )
+    # Backward pass
+    loss_activation.backward(loss_activation.output, y)
+    dense2. backward(loss_activation.dinputs)
+    activation1.backward(dense2.dinputs)
+    dense1. backward(activation1.dinputs)
+    # Update weights and biases
+    optimizer.pre_update_params()
+    optimizer.update_params(dense1)
+    optimizer.update_params(dense2)
+    optimizer.post_update_params()
--- trial.py
+++ trial.py
@@ -139,6 +139,8 @@ class Activation_Softmax_Loss_CategoricalCrossentropy () :
         self.dinputs[range(samples), y_true] -= 1
         # Normalize gradient
         self.dinputs = self.dinputs / samples
+
+# SGD Basic Optimizer code
 class Optimizer_SGD:
    
     # Initialize optimizer - set settings,
@@ -185,6 +187,7 @@ class Optimizer_SGD:
     def post_update_params (self):
         self.iterations += 1
 
+# AdaGrad Optimizer code
 class Optimizer_Adagrad:
    
     # Initialize optimizer - set settings,
@@ -219,6 +222,42 @@ class Optimizer_Adagrad:
     def post_update_params (self):
         self.iterations += 1
 
+# RMSprop Optimizer code
+class Optimizer_RMSprop:
+   
+    # Initialize optimizer - set settings,
+    # learning rate of 1. is default for this optimizer
+    def __init__(self, learning_rate=0.001, decay=0., epsilon=1e-7,rho=0.9):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+        self.rho = rho
+
+    # Call once before any parameter updates
+    def pre_update_params (self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1. / (1. + self. decay * self.iterations))
+    # Update parameters
+    def update_params (self, layer):
+        # If there is no cache array for weights
+        # create them
+        if not hasattr (layer,'weight_cache' ):
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+
+        # Update cache with squared current gradients
+        layer.weight_cache += self.rho * layer.weight_cache + ( 1-self.rho ) * layer.dweights**2
+        layer.bias_cache += self.rho * layer.bias_cache + ( 1-self.rho ) * layer.dbiases**2
+        # Vanilla SGD parameter update + normalization
+        # with square rooted cache
+        layer.weights += - self.current_learning_rate * layer.dweights / (np.sqrt(layer.weight_cache) + self.epsilon)
+        layer.biases += - self.current_learning_rate * layer.dbiases / (np.sqrt(layer.bias_cache) + self.epsilon)
+    # Call once after any parameter updates
+    def post_update_params (self):
+        self.iterations += 1
+
 # Create dataset
 X, y = spiral_data(samples=100, classes=3)
 # Create Dense layer with 2 input features and 64 output values
@@ -232,7 +271,7 @@ dense2 = Layer_Dense (64, 3)
 loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy ()
 # Create optimizer
 #optimizer = Optimizer_SGD(decay=1e-3, momentum=0.9)
-optimizer = Optimizer_Adagrad(decay=1e-4)
+#optimizer = Optimizer_Adagrad(decay=1e-4)
 # Train in 100p
 for epoch in range(10001):
     # Perform a forward pass of our training data through this layer
