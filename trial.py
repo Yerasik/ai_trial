50b28035b4a552fc1f794d9d447a3d4787314760
Yerasik <yerassylt@connect.hku.hk>
2025-09-24 14:31:06 +0800

    Testing the AdaGrad optimizer

--- trial.py
+++ trial.py
@@ -231,7 +231,8 @@ dense2 = Layer_Dense (64, 3)
 # Create Softmax classifier's combined loss and activation
 loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy ()
 # Create optimizer
-optimizer = Optimizer_SGD(decay=1e-3, momentum=0.9)
+#optimizer = Optimizer_SGD(decay=1e-3, momentum=0.9)
+optimizer = Optimizer_Adagrad(decay=1e-4)
 # Train in 100p
 for epoch in range(10001):
     # Perform a forward pass of our training data through this layer
