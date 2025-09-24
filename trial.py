98130aea93fb935a796371e7cadb3fd420a8c3b0
Yerasik <yerassylt@connect.hku.hk>
2025-09-24 14:29:06 +0800

    added the AdaGrad Optimizer implementation

--- trial.py
+++ trial.py
@@ -185,6 +185,40 @@ class Optimizer_SGD:
     def post_update_params (self):
         self.iterations += 1
 
+class Optimizer_Adagrad:
+   
+    # Initialize optimizer - set settings,
+    # learning rate of 1. is default for this optimizer
+    def __init__(self, learning_rate=1., decay=0., epsilon=1e-7):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+
+    # Call once before any parameter updates
+    def pre_update_params (self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1. / (1. + self. decay * self.iterations))
+    # Update parameters
+    def update_params (self, layer):
+        # If there is no cache array for weights
+        # create them
+        if not hasattr (layer,'weight_cache' ):
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+
+        # Update cache with squared current gradients
+        layer.weight_cache += layer.dweights ** 2
+        layer.bias_cache += layer.dbiases ** 2
+        # Vanilla SGD parameter update + normalization
+        # with square rooted cache
+        layer.weights += - self.current_learning_rate * layer.dweights / (np.sqrt(layer.weight_cache) + self.epsilon)
+        layer.biases += - self.current_learning_rate * layer.dbiases / (np.sqrt(layer.bias_cache) + self.epsilon)
+    # Call once after any parameter updates
+    def post_update_params (self):
+        self.iterations += 1
+
 # Create dataset
 X, y = spiral_data(samples=100, classes=3)
 # Create Dense layer with 2 input features and 64 output values
