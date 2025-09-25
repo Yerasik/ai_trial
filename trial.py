c019dfe9ac958c778a494a52f8dbb451828c1a6c
Yerasik <yerassylt@connect.hku.hk>
2025-09-25 14:27:57 +0800

    added the regulization implementation to the main cycle

--- trial.py
+++ trial.py
@@ -363,7 +363,7 @@ class Optimizer_Adam:
 # Create dataset
 X, y = spiral_data(samples=100, classes=3)
 # Create Dense layer with 2 input features and 64 output values
-dense1 = Layer_Dense (2, 64)
+dense1 = Layer_Dense (2, 64,weight_regularizer_l2 = 5e-4 ,bias_regularizer_l2 = 5e-4 )
 # Create ReLU activation (to be used with Dense layer):
 activation1 = Activation_ReLU()
 # Create second Dense layer with 64 input features (as we take output
@@ -375,7 +375,7 @@ loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy ()
 #optimizer = Optimizer_SGD(decay=1e-3, momentum=0.9)
 #optimizer = Optimizer_Adagrad(decay=1e-4)
 #optimizer = Optimizer_RMSprop(  learning_rate = 0.02, decay = 1e-5, rho=0.999 )
-optimizer = Optimizer_Adam( learning_rate = 0.05 , decay = 5e-7 )
+optimizer = Optimizer_Adam( learning_rate = 0.02 , decay = 5e-7 )
 # Train in 100p
 for epoch in range(10001):
     # Perform a forward pass of our training data through this layer
@@ -388,13 +388,13 @@ for epoch in range(10001):
     dense2.forward(activation1.output)
     # Perform a forward pass through the activation/loss function
     # takes the output of second dense layer here and returns loss
-    loss = loss_activation.forward(dense2.output, y)
-    # Calculate loss from output of activation2 so softmax activation
     data_loss = loss_function.forward(activation2.output, y)
+
     # Calculate regularization penalty
     regularization_loss = loss_function.regularization_loss(dense1) +loss_function.regularization_loss(dense2)
     # Calculate overall loss
     loss = data_loss + regularization_loss
+
     # Calculate accuracy from output of activation2 and targets
     # calculate values along first axis
     predictions = np.argmax(loss_activation.output, axis=1)
@@ -402,7 +402,7 @@ for epoch in range(10001):
         y = np.argmax (y, axis=1)
     accuracy = np.mean(predictions==y)
     if not epoch % 100:
-        print(f'epoch: {epoch}, ' + f'acc: {accuracy: 3f}, ' + f'loss: {loss: .3f},' +f'lr: {optimizer.current_learning_rate} ' )
+        print(f'epoch: {epoch}, ' + f'acc: {accuracy: 3f}, ' + f'loss: {loss: .3f},'+ f'reg_loss: {regularization_loss: .3f} ' +f'lr: {optimizer.current_learning_rate} ')
     # Backward pass
     loss_activation.backward(loss_activation.output, y)
     dense2. backward(loss_activation.dinputs)
@@ -413,3 +413,4 @@ for epoch in range(10001):
     optimizer.update_params(dense1)
     optimizer.update_params(dense2)
     optimizer.post_update_params()
+
