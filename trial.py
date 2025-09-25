2d22e3784eb162009ed77f7f8a4a9d03a08f153f
Yerasik <yerassylt@connect.hku.hk>
2025-09-25 14:01:27 +0800

    added the regulization variables to the dense layer

--- trial.py
+++ trial.py
@@ -5,10 +5,15 @@ nnfs.init()
 # Dense layer
 class Layer_Dense:
     # Layer initialization
-    def __init__(self, n_inputs, n_neurons):
+    def __init__(self, n_inputs, n_neurons,weight_regularizer_l1=0, weight_regularizer_l2=0, bias_regularizer_l1=0, bias_regularizer_l2=0):
         # Initialize weights and biases
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros( (1, n_neurons))
+        # Set regularization strength
+        self.weight_regularizer_l1 = weight_regularizer_l1
+        self.weight_regularizer_l2 = weight_regularizer_l2
+        self.bias_regularizer_l1 = bias_regularizer_l1
+        self.bias_regularizer_l2 = bias_regularizer_l2
     # Forward pass
     def forward (self, inputs):
         # Remember input values
