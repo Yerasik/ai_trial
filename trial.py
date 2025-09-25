f9209459a267965d433a88f72d2cc80938f86dca
Yerasik <yerassylt@connect.hku.hk>
2025-09-25 14:29:59 +0800

    added the validation

--- trial.py
+++ trial.py
@@ -414,3 +414,25 @@ for epoch in range(10001):
     optimizer.update_params(dense2)
     optimizer.post_update_params()
 
+# Validate the model
+# Create test dataset
+X_test, y_test = spiral_data( samples = 100 , classes = 3 )
+# Perform a forward pass of our testing data through this layer
+dense1.forward(X_test)
+# Perform a forward pass through activation function
+# takes the output of first dense layer here
+activation1.forward(dense1.output)
+# Perform a forward pass through second Dense layer
+# takes outputs of activation function of first layer as inputs
+dense2.forward(activation1.output)
+# Perform a forward pass through the activation/loss function
+# takes the output of second dense layer here and returns loss
+loss = loss_activation.forward(dense2.output, y_test)
+# Calculate accuracy from output of activation2 and targets
+# calculate values along first axis
+predictions = np.argmax(loss_activation.output, axis=1)
+if len(y.shape) == 2:
+    y = np.argmax (y, axis=1)
+accuracy = np.mean(predictions==y)
+if not epoch % 100:
+    print(f'acc: {accuracy: 3f}, ' + f'loss: {loss: .3f}')
