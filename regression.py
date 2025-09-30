c79e351b15fa9a024b990acec7305a585f53023a
Yerasik <yerassylt@connect.hku.hk>
2025-09-30 16:26:09 +0800

    added a new Linear Activation layer for the regression model

--- regression.py
+++ regression.py
@@ -4,3 +4,17 @@ from nnfs.datasets import sine_data
 
 nnfs.init()
 X, y = sine_data()
+plt.plot(X, y)
+plt.show()
+
+# Linear activation
+class Activation_Linear:
+    # Forward pass
+    def forward (self, inputs):
+        # Just remember values
+        self.inputs = inputs
+        self.output = inputs
+    # Backward pass
+    def backward (self, dvalues):
+        # derivative is 1, 1 * dvalues = dvalues - the chain rule
+        self.dinputs = dvalues.copy()
