3484a84c0f03cef51bc5d0a4c992b8c720196d41
Yerasik <yerassylt@connect.hku.hk>
2025-09-30 17:43:51 +0800

    added a third layer to overcome the local minimum problem

--- regression.py
+++ regression.py
@@ -206,17 +206,15 @@ class Optimizer_Adam:
 X, y = sine_data()
 # Create Dense layer with 1 input feature and 64 output values
 dense1 = Layer_Dense (1, 64)
-# Create ReLU activation (to be used with Dense layer):
 activation1 = Activation_ReLU()
-# Create second Dense layer with 64 input features (as we take output
-# of previous layer here) and 1 output value
-dense2 = Layer_Dense (64, 1)
-# Create Linear activation:
-activation2 = Activation_Linear()
+dense2 = Layer_Dense (64, 64)
+activation2 = Activation_ReLU()
+dense3 = Layer_Dense (64, 1)
+activation3= Activation_Linear()
 # Create loss function
 loss_function = Loss_MeanSquaredError ()
 # Create optimizer
-optimizer = Optimizer_Adam()
+optimizer = Optimizer_Adam(learning_rate = 0.05 , decay = 1e-3)
 # Accuracy precision for accuracy calculation
 # There are no really accuracy factor for regression problem,
 # but we can simulate/approximate it. We'll calculate it by checking
@@ -228,40 +226,47 @@ accuracy_precision = np.std(y) / 250
 
 # Train in loop
 for epoch in range ( 10001 ):
-    # Perform a forward pass of our training data through this layer
     dense1.forward(X)
-    # Perform a forward pass through activation function
-    # takes the output of first dense layer here
     activation1.forward(dense1.output)
-    # Perform a forward pass through second Dense layer
-    # takes outputs of activation function
-    # of first layer as inputs
     dense2.forward(activation1.output)
-    # Perform a forward pass through activation function
-    # takes the output of second dense layer here
     activation2.forward(dense2.output)
+    dense3.forward(activation2.output)
+    activation3.forward(dense3.output)
     # Calculate the data loss
-    data_loss = loss_function.calculate(activation2.output, y)
+    data_loss = loss_function.calculate(activation3.output, y)
     # Calculate regularization penalty
-    regularization_loss = loss_function.regularization_loss (dense1) + loss_function.regularization_loss(dense2)
+    regularization_loss = loss_function.regularization_loss (dense1) + loss_function.regularization_loss(dense2) + loss_function.regularization_loss(dense3)
     # Calculate overall loss
     loss = data_loss + regularization_loss
     # Calculate accuracy from output of activation2 and targets
     # To calculate it we're taking absolute difference between
     # predictions and ground truth values and compare if differences
     # are lower than given precision value
-    predictions = activation2.output
+    predictions = activation3.output
     accuracy = np.mean(np.absolute(predictions -y) < accuracy_precision)
     if not epoch % 100:
         print(f'epoch: {epoch}, ' + f'acc: {accuracy:.3f},' + f'loss: {loss:.3f} (' + f'data_loss: {data_loss: .3f},' + f'reg_loss: {regularization_loss:.3f}),' + f'lr: {optimizer.current_learning_rate}')
     # Backward pass
-    loss_function.backward(activation2.output, y) 
-    activation2.backward(loss_function.dinputs) 
+    loss_function.backward(activation3.output, y) 
+    activation3.backward(loss_function.dinputs) 
+    dense3.backward(activation3.dinputs)
+    activation2.backward(dense3.dinputs) 
     dense2.backward(activation2.dinputs)
     activation1.backward(dense2.dinputs)
     dense1.backward(activation1.dinputs)
     # Update weights and biases
     optimizer.pre_update_params()
     optimizer. update_params (dense1) 
-    optimizer. update_params(dense2) 
+    optimizer. update_params(dense2)
+    optimizer. update_params(dense3) 
     optimizer.post_update_params()
+x_test, y_test = sine_data()
+dense1.forward(x_test)
+activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+dense3.forward(activation2.output)
+activation3.forward(dense3.output)
+plt.plot(x_test, y_test)
+plt.plot(x_test, activation3.output)
+plt.show()
