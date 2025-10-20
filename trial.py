8399ea44b99b6948022ba6fa97a6e22283ab69d3
Yerasik <yerassylt@connect.hku.hk>
2025-10-20 17:44:12 +0800

    started work on a real data set

--- trial.py
+++ trial.py
@@ -1,5 +1,19 @@
-import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
-files = os.listdir( 'fashion_mnist_images/train/0')
-print (files[:10])
-print (len(files))
+# Scan all the directories and create a list of labels
+labels = os.listdir( 'fashion_mnist_images/train')
+# Create lists for samples and labels
+X = []
+y = []
+# For each label folder
+for label in labels:
+  # And for each image in given folder
+  for file in os.listdir(os.path.join('fashion_mnist_images','train',label)):
+    # Read the image
+    image = cv2.imread(os.path.join('fashion_mnist_images/train', label, file), cv2.IMREAD_UNCHANGED)
+    # And append it and a label to the lists
+    X.append(image)
+    y.append(label)
+
