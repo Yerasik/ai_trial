13527b056f18f44908f85ad6c15ecfec30d0c380
Yerasik <yerassylt@connect.hku.hk>
2025-10-22 20:21:38 +0800

    did the same for accuracy

--- trial.py
+++ trial.py
@@ -309,60 +309,60 @@ class Layer_Dropout:
 # Common loss class
 class Loss:
 
-    # Regularization loss calculation
-    def regularization_loss ( self ):
-        # 0 by default
-        regularization_loss = 0
+  # Regularization loss calculation
+  def regularization_loss ( self ):
+      # 0 by default
+      regularization_loss = 0
 
-        
-        for layer in self.trainable_layers:
+      
+      for layer in self.trainable_layers:
 
-            # Calculate only if the layer parameter is greater than o
-            # L1 regulization
-            if layer.weight_regularizer_l1>0:
-                regularization_loss += layer.weight_regularizer_l1 * np.sum(np.abs(layer.weights))
+          # Calculate only if the layer parameter is greater than o
+          # L1 regulization
+          if layer.weight_regularizer_l1>0:
+              regularization_loss += layer.weight_regularizer_l1 * np.sum(np.abs(layer.weights))
 
-            # L2 regulization
-            if layer.weight_regularizer_l2>0:
-                regularization_loss += layer.weight_regularizer_l2*np.sum(layer.weights*layer.weights)
-            
-            # L1 regulization biases
-            if layer.bias_regularizer_l1>0:
-                regularization_loss += layer.bias_regularizer_l1*np.sum(np.abs(layer.biases))
+          # L2 regulization
+          if layer.weight_regularizer_l2>0:
+              regularization_loss += layer.weight_regularizer_l2*np.sum(layer.weights*layer.weights)
+          
+          # L1 regulization biases
+          if layer.bias_regularizer_l1>0:
+              regularization_loss += layer.bias_regularizer_l1*np.sum(np.abs(layer.biases))
 
-            # L2 regulization biases
-            if layer.bias_regularizer_l2>0:
-                regularization_loss += layer.bias_regularizer_l2*np.sum(layer.biases*layer.biases)
-        
-        return regularization_loss
+          # L2 regulization biases
+          if layer.bias_regularizer_l2>0:
+              regularization_loss += layer.bias_regularizer_l2*np.sum(layer.biases*layer.biases)
+      
+      return regularization_loss
 
-    # Set/remember trainable layers
-    def remember_trainable_layers (self, trainable_layers):
-        self.trainable_layers = trainable_layers
-    # Calculates the data and regularization losses
-    # given model output and ground truth values
-    def calculate (self, output, y, *, include_regularization = False) :
-        # Calculate sample losses
-        sample_losses = self.forward (output, y)
-        # Calculate mean loss
-        data_loss = np.mean(sample_losses)
-
-        # Add accumulated sum of losses and sample count
-        self.accumulated_sum +=np.sum(sample_losses)
-        self.accumulated_count +=len(sample_losses)
-        # If just data loss - return it
-        if not include_regularization:
-            return data_loss
-
-        # Return loss
-        return data_loss, self.regularization_loss()
-    #Calculates accumulated loss
-    def calculate_accumulated ( self ,*, include_regularization = False ):
+  # Set/remember trainable layers
+  def remember_trainable_layers (self, trainable_layers):
+      self.trainable_layers = trainable_layers
+  # Calculates the data and regularization losses
+  # given model output and ground truth values
+  def calculate (self, output, y, *, include_regularization = False) :
+      # Calculate sample losses
+      sample_losses = self.forward (output, y)
       # Calculate mean loss
-      data_loss = self.accumulated_sum / self.accumulated_count
+      data_loss = np.mean(sample_losses)
+
+      # Add accumulated sum of losses and sample count
+      self.accumulated_sum +=np.sum(sample_losses)
+      self.accumulated_count +=len(sample_losses)
       # If just data loss - return it
       if not include_regularization:
-        return data_loss
+          return data_loss
+
+      # Return loss
+      return data_loss, self.regularization_loss()
+    #Calculates accumulated loss
+  def calculate_accumulated ( self ,*, include_regularization = False ):
+    # Calculate mean loss
+    data_loss = self.accumulated_sum / self.accumulated_count
+    # If just data loss - return it
+    if not include_regularization:
+      return data_loss
 
       # Return the data and regularization losses
       return data_loss, self.regularization_loss()
@@ -491,15 +491,30 @@ class Loss_MeanAbsoluteError (Loss): # L1 loss
 
 # Common accuracy class
 class Accuracy:
-    # Calculates an accuracy
-    # given predictions and ground truth values
-    def calculate (self, predictions, y):
-        # Get comparison results
-        comparisons = self.compare (predictions, y)
-        # Calculate an accuracy
-        accuracy = np.mean(comparisons)
-        # Return accuracy
-        return accuracy
+  # Calculates an accuracy
+  # given predictions and ground truth values
+  def calculate (self, predictions, y):
+    # Get comparison results
+    comparisons = self.compare (predictions, y)
+    # Calculate an accuracy
+    accuracy = np.mean(comparisons)
+
+    # Add accumulated sum of matching values and sample count
+    self.accumulated_sum +=np.sum(comparisons)
+    self.accumulated_count +=len(comparisons)
+    # Return accuracy
+    return accuracy
+  def calculate_accumulated ( self  ):
+    # Calculate mean loss
+    accuracy = self.accumulated_sum / self.accumulated_count
+    # Return the data and regularization losses
+    return accuracy
+
+ 
+  # Reset variables for accumulated loss
+  def new_pass ( self ):
+    self.accumulated_sum=0
+    self.accumulated_count=0
 
 # Accuracy calculation for classification model
 class Accuracy_Categorical(Accuracy):
